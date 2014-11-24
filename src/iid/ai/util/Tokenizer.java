69b067f77f34d63e962208ada4f68c4f0a3c2dfe
ramononis <ramononis@hotmail.com>
2014-11-24 10:02:01 +0100

    first commit

---
+++ src/iid/ai/util/Tokenizer.java
@@ -0,0 +1,15 @@
+package iid.ai.util;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Tokenizer extends StringTokenizer {
+
+	public Tokenizer(String str) {
+		super(str);
+	}
+	public String nextToken() {
+		return super.nextToken().replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+	}
+}
